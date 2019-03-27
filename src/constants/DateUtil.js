d5ecaad26d0f2ac1d80d31cb4deda91fc11e1f15
suishanwen <suishanwen@icloud.com>
2019-03-27 17:26:37 +0800

    时间格式化

--- src/constants/DateUtil.js
+++ src/constants/DateUtil.js
@@ -1,5 +1,11 @@
-class DateUtil{
+const reDateTime = /^(?:19|20)[0-9][0-9]-(?:(?:0[1-9])|(?:1[0-2]))-(?:(?:[0-2][1-9])|(?:[1-3][0-1])) (?:(?:[0-2][0-3])|(?:[0-1][0-9])):[0-5][0-9]:[0-5][0-9]$/;
+
+class DateUtil {
+
     static formatDateTime(inputTime) {
+        if (reDateTime.test(inputTime)) {
+            return inputTime;
+        }
         let date = new Date(inputTime);
         let y = date.getFullYear();
         let m = date.getMonth() + 1;
@@ -12,7 +18,7 @@ class DateUtil{
         let second = date.getSeconds();
         minute = minute < 10 ? ('0' + minute) : minute;
         second = second < 10 ? ('0' + second) : second;
-        return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;
+        return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;
     };
 }
 
