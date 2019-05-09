3b1b04f8dbe142e20434cc52ff6b8be63f9b8514
suishanwen <suishanwen@icloud.com>
2019-05-09 14:11:14 +0800

    fix scroll

--- src/routes/edit/index.js
+++ src/routes/edit/index.js
@@ -107,12 +107,12 @@ class Edit extends React.Component {
         const {rem} = window;
         window["noteEditor"] = new window.TQEditor('content', {
             toolbar: 'full',
-            imageUploadUrl: 'https://bitcoinrobot.cn:8052/sw/api/file/upload',
+            imageUploadUrl: 'https://bitcoinrobot.cn/api/file/upload',
             directInsertUploadImage: true,
-            flashUploadUrl: 'https://bitcoinrobot.cn:8052/sw/api/file/upload',
-            videoUploadUrl: 'https://bitcoinrobot.cn:8052/sw/api/file/upload',
-            musicUploadUrl: 'https://bitcoinrobot.cn:8052/sw/api/file/upload',
-            linkUploadUrl: 'https://bitcoinrobot.cn:8052/sw/api/file/upload'
+            flashUploadUrl: 'https://bitcoinrobot.cn/api/file/upload',
+            videoUploadUrl: 'https://bitcoinrobot.cn/api/file/upload',
+            musicUploadUrl: 'https://bitcoinrobot.cn/api/file/upload',
+            linkUploadUrl: 'https://bitcoinrobot.cn/api/file/upload'
         });
         const container = $("#TQEditorContainer_content")[0];
         let width = `${808 / 54 * rem}`;
